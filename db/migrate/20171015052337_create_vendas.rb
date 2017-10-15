ea1db0a78ad0207215156a1addd855c85a66d974
marviorocha <marviorocha@marviorocha.com>
2017-10-15 03:25:12 -0200

    Fix Database migrate!

--- db/migrate/20171015052337_create_vendas.rb
+++ db/migrate/20171015052337_create_vendas.rb
@@ -1,11 +1,11 @@
 class CreateVendas < ActiveRecord::Migration[5.1]
   def change
     create_table :vendas do |t|
-      t.string, :purchaser_name
-      t.text, :item_description
-      t.interagir, :item_price
-      t.interagir, :purchase_count
-      t.text, :merchant_address
+      t.string :purchaser_name
+      t.text :item_description
+      t.integer :item_price
+      t.integer :purchase_count
+      t.text :merchant_address
       t.string :merchant_name
 
       t.timestamps
